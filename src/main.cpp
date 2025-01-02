45a5fb5cbf82ae9575c6a2c2a3b40c0837d457cd
DocKenny <oskar.forstnairc@student.um.si>
2025-01-02 15:31:11 +0100

    Changed to MQTTPubSubClient

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,38 @@
 #include <Arduino.h>
 #include <WiFi.h>
-#include <PubSubClient.h>
+#include <MQTTPubSubClient.h>
 
 #include "../include/config.h"
+
+void MQTTcallback(char* topic, byte* payload, unsigned int length);
+
+WiFiClient wifiClient;
+MQTTPubSubClient mqtt;
+
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
 
-  WiFi.begin("SSID", "PASSWORD");
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Connecting to WiFi..");
-  }
-  Serial.println("Connected to the WiFi network");
-
+  
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   Serial.println("Hello World!");
+
+  while(!espClient.connected()) {
+
+  }
+}
+
+void MQTTcallback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
 }
