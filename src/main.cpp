b53be66425d83b8905b8f52d12e87582caf7d40f
DocKenny <oskar.forstnairc@student.um.si>
2025-01-02 15:14:29 +0100

    Added PubSubClient library for MQTT

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,20 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
+
+  WiFi.begin("SSID", "PASSWORD");
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
+  }
+  Serial.println("Connected to the WiFi network");
+
+
 }
 
 void loop() {
