f7ba78479decdf44dfa91135bf477c999b2ac103
DocKenny <oskar.forstnairc@student.um.si>
2025-01-03 16:14:39 +0100

    Add example config file

---
+++ include/config-example.h
@@ -0,0 +1,11 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define WIFI_SSID ""
+#define WIFI_PASSWORD ""
+
+#define MQTT_SERVER ""
+#define MQTT_PORT 9001
+#define MQTT_USER "ESP32Client"
+
+#endif // CONFIG_H
