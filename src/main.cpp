5fe0fdf2811638c16c30c711c9c1b3ea2d9bf19c
DocKenny <oskar.forstnaric@gmail.com>
2025-01-05 21:33:56 +0100

    ToF senzor

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include <WiFi.h>
 #include <MQTTPubSubClient.h>
 #include <HardwareSerial.h>
+#include <Wire.h>
+#include "Adafruit_VL53L0X.h"
 
 #include "../include/config.h"
 
@@ -12,13 +14,28 @@ void connectToMQTT();
 WiFiClient wifiClient;
 MQTTPubSubClient mqtt;
 HardwareSerial mySerial(1);
+// Create an instance of the sensor
+Adafruit_VL53L0X tof = Adafruit_VL53L0X();
 
 #define RX_PIN 16 
 #define TX_PIN 17  
+#define I2C_SDA 20
+#define I2C_SCL 21
 
 void setup() {
   Serial.begin(115200);
   mySerial.begin(9600, SERIAL_8N1, RX_PIN, TX_PIN);
+  mySerial.println("test");
+  Wire.begin(I2C_SDA, I2C_SCL);
+
+   // Initialize the sensor with the new I2C address
+  if (!tof.begin(0x29)) {
+    mySerial.println("Failed to find the sensor");
+    while (1);  // Halt execution if sensor is not found
+  }
+
+  mySerial.println("Sensor Initialized!");
+
   connectToWifi();
 
   wifiClient.connect(MQTT_SERVER, MQTT_PORT);
@@ -32,6 +49,9 @@ void setup() {
     });
 
   mqtt.publish("esp32", "Hello from ESP32");
+  
+
+  mySerial.println("test");
 }
 
 void loop() {
@@ -45,11 +65,20 @@ void loop() {
 
   mqtt.update();
   mySerial.println("loop");
+  VL53L0X_RangingMeasurementData_t measure;
 
-  if(Serial.available()) {
-    String message = mySerial.readString();
-    mqtt.publish("esp32", message);
+  // Read the sensor data
+  tof.rangingTest(&measure, false);
+  
+  // Display the distance measurement
+  if (measure.RangeStatus != 4) {  // 4 means out of range
+    Serial.print("Distance (mm): ");
+    Serial.println(measure.RangeMilliMeter);
+  } else {
+    Serial.println("Out of range");
   }
+  
+  delay(100);  
 }
 
 void MQTTcallback(char* topic, byte* payload, unsigned int length) {
